80fe37a8366217330c91ac0d4751b355ae240fcd
Your Name <you@example.com>
2022-11-02 17:58:30 +0000

    latest

---
+++ lib/types.d.ts
@@ -0,0 +1,11 @@
+export declare enum UpOrDown {
+    None = 0,
+    Up = 1,
+    Down = 2
+}
+export declare enum Oracle {
+    Undefined = 0,
+    Chainlink = 1,
+    Pyth = 2,
+    Switchboard = 3
+}
---
+++ src/types.ts
@@ -0,0 +1,12 @@
+export enum UpOrDown {
+    None = 0,
+    Up = 1,
+    Down = 2
+}
+
+export enum Oracle {
+    Undefined = 0,
+    Chainlink = 1,
+    Pyth = 2,
+    Switchboard = 3
+}
